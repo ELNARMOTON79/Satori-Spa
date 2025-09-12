0f8580cdd255fe2f1a1493a051f9a8564f9baaf5
Luis Angel Alaniz <luisangelazm@gmail.com>
2025-09-12 09:51:23 -0600

    Implementacion de Dashboard

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'config/database.php';
+require_once 'views/admin_view.html';
 
 // Basic router
 $url = isset($_GET['url']) ? rtrim($_GET['url'], '/') : 'home';
@@ -8,6 +9,13 @@ $url = filter_var($url, FILTER_SANITIZE_URL);
 $url = explode('/', $url);
 
 // You can expand this to load controllers and methods
+
+$data = getDashboardData();
+
+// Permite usar las variables PHP dentro de la vista HTML
+ob_start();
+include 'views/admin_view.html';
+echo ob_get_clean();
 
 echo "<pre>";
 print_r($url);
